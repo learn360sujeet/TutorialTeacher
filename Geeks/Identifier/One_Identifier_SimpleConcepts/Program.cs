fc88d07d0124869a61d1bd377e8756c1b0038ca6
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-25 19:11:01 +0530

    Identifiers Concept

---
+++ Geeks/Identifier/One_Identifier_SimpleConcepts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Name
+{
+    /// <summary>
+    /// Identifiers in this language is class name ,method name and variable name 
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        string elderBrotherName = "Sujeet";
+        int brothersCount = 3;
+        Console.WriteLine("Hello {0},\"Your brother's Count is  {1} :", elderBrotherName,  brothersCount);
+        Console.ReadLine();
+    }
+}
